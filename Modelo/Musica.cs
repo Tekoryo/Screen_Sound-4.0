82b83281ed3853685c0e6cb57b284cfa378bd03a
Thiago Coelho <thiagocoelho.coelho2@gmail.com>
2023-08-30 16:34:21 -0300

    Criando uma classe para colocar as musica

---
+++ Modelo/Musica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Screen_Sound_4._0.Modelo;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalheDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Musica: {Nome}");
+        Console.WriteLine($"Duração em segundo: {Duracao/1000}");
+        Console.WriteLine($"Genero: {Genero}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using Screen_Sound_4._0.Modelo;
+using System.Text.Json;
+
+/*
+ Exibir todos os gêneros musicais da lista;
+
+Ordenar os artistas por nome;
+
+Filtrar artistas por gênero musical;
+
+Filtrar as músicas de um artista.
+ */
+
+using (HttpClient client = new HttpClient())
 {
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(resposta);
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalheDaMusica();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
 }
